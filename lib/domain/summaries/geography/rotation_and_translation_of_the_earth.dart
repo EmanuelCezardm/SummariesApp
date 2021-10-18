625b52e5d92918e57c02235bea22e6201164562d
Angelina <anso1@aluno.ifal.edu.br>
2021-10-18 10:08:30 -0300

    atualizacaoResumosGeo

---
+++ lib/domain/summaries/geography/rotation_and_translation_of_the_earth.dart
@@ -0,0 +1,41 @@
+final List<Map<String, dynamic>> resumoRotacaoTranslacao = [
+  {
+    'text': ' Movimentos de rotação e translação da Terra',
+    'id_type': 0,
+  },
+  {
+    'text':
+        ' A rotação e a translação formam os dois mais importantes movimentos terrestres, pois são eles que provocam os efeitos mais notórios sobre o nosso planeta.',
+    'id_type': 2,
+  },
+  {
+    'text':
+        'Rotação e translação são os dois principais e mais conhecidos movimentos realizados pelo planeta Terra. ',
+    'id_type': 2,
+  },
+  {
+    'text':
+        'Juntos, eles são responsáveis por uma infinidade de fenômenos que se manifestam na atmosfera e na litosfera, interferindo no clima, no relevo e até na duração dos dias e das noites.',
+    'id_type': 2,
+  },
+  {
+    'text':
+        '  A rotação é o movimento que a Terra realiza em torno de seu próprio eixo, é como se ela estivesse “rodando” em volta de si mesma. O tempo que o planeta leva para completar esse “giro” é de 24 horas. A principal consequência é a existência alternada entre os dias e as noites, pois, se não houvesse esse movimento, haveria apenas dia em um lado do planeta (que seria extremamente quente) e apenas noite no outro lado (que seria extremamente frio).',
+    'id_type': 2,
+  },
+  {
+    'text':
+        'A translação é o movimento que a Terra realiza em torno do Sol, sendo que ela demora 365 dias, 5 horas e 48 minutos para completá-lo. Esse movimento é o responsável direto pela existência das estações do ano. ',
+    'id_type': 2,
+  },
+  {
+    'text':
+        'Como o eixo de inclinação do nosso planeta é de 23º27, há períodos em que os dias são maiores que as noites (solstícios de verão), períodos em que as noites são maiores que os dias (solstícios de inverno) e períodos em que eles possuem a mesma duração (equinócios de primavera e outono).',
+    'id_type': 2,
+  },
+  {
+    'text':
+        'FONTE: https://brasilescola.uol.com.br/o-que-e/geografia/o-que-e-rotacao-translacao.htm',
+    'id_type': 5,
+  },
+];
