0f4f89b33506c099fbc70e61a8becbddc45ecff1
Angelina <anso1@aluno.ifal.edu.br>
2021-10-19 23:08:55 -0300

    Atualizacao resumos Biologia e Portugues

---
+++ lib/domain/summaries/portuguese/syntax_figure.dart
@@ -0,0 +1,71 @@
+final List<Map<String, dynamic>> resumoFiguraDeSintaxe[
+	{
+		'text':'Figuras de Sintaxe',
+		'id_type':1,
+	},
+	{
+		'text':'Eclipse - Quando termos que foram omitidos podem ser identificados a partir do contexto de texto.',
+		'id_type':3,
+	},
+	{
+		'text':'	Exemplo - “Eras na vida a pomba predileta.”, o “tu” é identificável mesmo não sendo expresso anteriormente.',
+		'id_type':2,
+	},
+	{
+		'text':'Zeugma - É um tipo de eclipse que ocorre quando a unidade linguística ou de um sintagma já ocorreu num enunciado.',
+		'id_type':3,
+	},
+	{
+		'text':'Exemplo - “ Fabiana comeu maçã, eu (comi) pera.”',
+		'id_type':2,
+	},
+	{
+		'text':'Anacoluto - Consiste na alteração da estrutura regular de uma oração, a partir do uso de uma palavra ou expressão que permanece "solta", sem conexões sintáticas com outros termos da oração.',
+		'id_type':3,
+	},
+	{
+		'text':'Exemplo - “Esses políticos de hoje, não se pode confiar.”',
+		'id_type':2,
+	},
+	{
+		'text':'Anáfora - Ocorre a repetição de termos no início de cada membro sucessivo de frase, texto, prosa ou verso.',
+		'id_type':3,
+	},
+	{
+		'text':'Exemplo: “Se eu amasse, se eu chorasse, se eu perdoasse.”',
+		'id_type':2,
+	},
+	{
+		'text':'Hipérbato - Consiste na inversão de termos ou orações num período.',
+		'id_type':3,
+	},
+	{
+		'text':'Exemplo: “Um DVD, Paulo comprou ontem”',
+		'id_type':2,
+	},
+	{
+		'text':'Polissíndeto - Repetição de vários termos da oração por meio de conjunções.',
+		'id_type':3,
+	},
+	{
+		'text':'Exemplo: “Dolores brigava, e gritava, e falava.”',
+		'id_type':2,
+	}
+	{
+		'text':'Assíndeto - Ocorre quando há a ausência de elemento conector numa sequência cumulativa.',
+		'id_type':3,
+	}
+	{
+		'text':'Exemplo: “Daiana comprou uvas para comer, limões para fazer suco", é possível notar a falta do “e” em “Daiana comprou uvas para comer, (e) limões para fazer suco”.',
+		'id_type':2,
+	}
+	{
+		'text':'Pleonasmo - Ocorre ao tentar enfatizar alguma ideia, utilizando termos desnecessários.',
+		'id_type':3,
+	}
+	{
+		'text':'Exemplo: “Vi, com os meus olhos, uma onça pintada”',
+		'id_type':2,
+	}
+];
+	
---
+++ lib/domain/summaries/portuguese/thought_figure.dart
@@ -0,0 +1,78 @@
+final List<Map<String, dynamic>> resumoFuguraDeLinguagem[
+	{
+		'text':'Figuras de Linguagem',
+		'id_type':1,
+	},
+	{
+		'text':'Hipérbole - Ocorre ao se referir a algo de forma exagerada.',
+		'id_type':3,
+	},
+	{
+		'text':'Exemplo: “Chorou rios de lágrimas.”',
+		'id_type':2,
+	},
+	{
+		'text':'Eufemismo - Ocorre quando uma palavra desagradável é substituída por outra que suaviza o enunciado.',
+		'id_type':3,
+	},
+	{
+		'text':'Exemplo: “O pai de Ricardo sempre soube que o filho tinha um caráter duvidoso.”',
+		'id_type':2,
+	},
+	{
+		'text':'Prosopopeia ou Personificação - Consiste na atribuição, ou transferência, de características de seres animados a seres inanimados.',
+		'id_type':3,
+	},
+	{
+		'text':'Exemplo: “O jardim olhava as crianças sem dizer nada.”',
+		'id_type':2,
+	},
+	{
+		'text':'Antítese - São associações contrárias feitas nos textos, dando um sentido antônimo.',
+		'id_type':3,
+	},
+	{
+		'text':'Exemplo: “A relação deles era de amor e ódio.”',
+		'id_type':2,
+	},
+	{
+		'text':'Paradoxo - Consiste na associação de termos contraditórios que se referem a algo.',
+		'id_type':3,
+	},
+	{
+		'text':'Exemplo: “Eu estou cheio de me sentir vazio.”',
+		'id_type':2,
+	},
+	{
+		'text':'Gradação - É a ocorrência de uma sequência que estabelece uma progressão com o passar dos processos, podendo ser de forma crescente ou decrescente.',
+		'id_type':3,
+	},
+	{
+		'text':'Exemplo: “É pau, é pedra, é o fim do caminho.”',
+		'id_type':2,
+	},
+	{
+		'text':'Apóstrofe  - É quando ocorre uma apelação pessoal por uma pessoa real ou imaginária, presente ou ausente.',
+		'id_type':3,
+	},
+	{
+		'text':'Exemplo: “Ó meu querido Santo António”',
+		'id_type':2,
+	},
+	{
+		'text':'Referências:',
+		'id_type':3,
+	},
+	{	
+		'text':'https://mundoeducacao.uol.com.br/gramatica/figuras-linguagemparte-iii.htm',
+		'id_type':5,
+	},
+	{
+		'text':'https://pt.calameo.com/read/002899327289d21dd4ac6?authid=mpRdTMc0AFTW',
+		'id_type':5,
+	},
+];
+
+
+
+
